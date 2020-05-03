3c950000e3fdd8a770c4d13d4da6ec8a7d519662
lenjavollmer <lenja.vollmer@gmx.de>
2020-05-03 20:02:06 +0000

    blatt2_aufgabe2a,b

--- stichprobe2.cc
+++ stichprobe2.cc
@@ -37,7 +37,7 @@ fin.close(); //Eingabedatei schließen
 fout.close(); //Ausgabedatei schließen
 
 
-//Aufgabe 2b: Varianzen
+//Aufgabe 2a: Varianzen
 
 fin.open("datensumme.txt"); //erneut aus datensumme.txt einlesen
 fout.open("varianzen.txt"); //in varianzen.txt ausgeben
@@ -79,5 +79,47 @@ for (int j=1;j<=26;j++) { //Schleife für die 26 Varianzen beginnen
 fout.close(); //Ausgabedatei schließen
 fin.close(); //Eingabedatei schließen
 fin2.close(); //weitere Eingabedatei schließen
+
+
+//Start Aufgabe 2b
+
+ifstream fin3("mittelwerte.txt"); //aus mittelwerte.txt einlesen
+
+N=0; //Variable N zurücksetzen
+sm=0; //Variable sm zurücksetzen
+a=0; //Variable a zurücksetzen
+
+for (int l=1;l<=26;l++) { //Schleife zum Füllen der Variable mit Werten aus Zeile 1 bis 26 in ganzen Schritten
+
+  fin3 >> N; //Wert an l'ter Stelle in Variable N schreiben
+  sm+=N; //Summe der Einträge in mittelwerte.txt berechnen
+
+}; //Schleife beenden
+
+a=(sm/26.0); //Summe durch Anzahl der Einträge teilen und in a schreiben
+
+cout << a << endl; //Mittelwert ausgeben
+
+fin3.close(); //Eingabedatei schließen
+
+ifstream fin4("varianzen.txt"); //aus varianzen.txt einlesen
+
+V=0; //Variable V zurücksetzen
+N=0; //Variable N zurücksetzen
+sm=0; //Variable sm zurücksetzen
+
+for (int m=1;m<=26;m++) { //Schleife zum Füllen der Variable mit Werten aus Zeile 1 bis 26 in ganzen Schritten
+
+  fin4 >> N; //Werte an m'ter Stelle in Variable N schreiben
+  sm+=pow(N-a, 2); //Summe der Quadrate von N minus dem Mittelwert berechnen
+
+}; //Schleife beenden
+
+V=(sm/26.0); //Summe durch Anzahl der Einträge teilen und in V schreiben
+
+cout << V << endl; //Varianz ausgeben
+
+fin4.close(); //Eingabedatei schließen
+
 
 } ; //beende Funktion
