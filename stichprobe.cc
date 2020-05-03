2fcdde89d869d0ee40b064188acb333843e742bb
lenjavollmer <lenja.vollmer@gmx.de>
2020-05-03 14:36:08 +0000

    blatt2_aufgabe1a,b

--- stichprobe.cc
+++ stichprobe.cc
@@ -1,14 +1,18 @@
 #include <iostream> //Ausgabe ins Terminal
 #include <fstream> //Eingabe aus Datei
+#include <cmath> //Mathefunktionen
 using namespace std; 
 
 int main () { //beginne Funktion
 
+
+//Start Aufgabe 1a
+
 ifstream fin ("datensumme.txt"); //aus datensumme.txt einlesen
 
-int N=0; //Definieren der Variable N
+double N=0; //Definieren der Variable N
 double a=0; //Definieren der Variable a für den Mittelwert
-int sm=0; //Definieren der Variable sm
+double sm=0; //Definieren der Variable sm
 
 for (int i=1;i<=234;i++) { //Schleife zum Füllen der Variable mit Werten aus Zeile 1 bis 234 in ganzen Schritten
 
@@ -21,5 +25,29 @@ a=(sm/234.0); //Summe durch Anzahl der Einträge teilen und in a schreiben
 
 cout << a << endl; //Mittelwert ausgeben
 
+fin.close(); //Eingabedatei schließen 
+
+
+//Start Aufgabe 1b: Varianz
+
+fin.open("datensumme.txt"); //erneut aus datensumme.txt einlesen
+
+double V=0; //Definieren der Variable V für die Varianz
+N=0; //Variable N zurücksetzen
+sm=0; //Variable sm zurücksetzen
+
+for (int i=1;i<=234;i++) { //Schleife zum Füllen der Variable mit Werten aus Zeile 1 bis 234 in ganzen Schritten
+
+fin >> N; //Werte an i'ter Stelle in Variable N schreiben
+sm+=pow(N-a, 2); //Summe der Quadrate von N minus dem Mittelwert berechnen
+
+
+}; //Schleife beenden
+
+V=(sm/234.0); //Summe durch Anzahl der Einträge teilen und in V schreiben
+
+cout << V << endl; //Varianz ausgeben
+
+fin.close(); //Eingabedatei schließen
 
 } ; //beende Funktion
